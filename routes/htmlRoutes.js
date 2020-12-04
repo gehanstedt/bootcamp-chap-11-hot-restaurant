9cc62f3cb5dca67fbffdbe9b206a2c7828b03e1d
G Dog <greg.hanstedt@med.ge.com>
2020-12-03 23:01:28 -0500

    Adjusted comments in console.log messages in routes/htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,17 +8,18 @@ module.exports = function(app) {
     // Route for /tables to display current reservations
     app.get("/tables", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/tables.html"));
-        console.log (`User requested main.html via "/tables" route.`);
+        console.log (`User requested tables.html via "/tables" route.`);
     });
       
     // Route for /reserve to show the reservation form
     app.get("/reserve", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/reserve.html"));
+        console.log (`User requested reserve.html via "/reserve" route.`);
     });
 
     // Default route if any other URL is selected to display the main.html page
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/main.html"));
-        console.log (`User requested main.html via "/" route.`);
+        console.log (`User requested main.html via "*" route.`);
     });
 }
